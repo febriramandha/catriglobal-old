b2b890a109c65e9469ce7eb7e8cd0ae6964f010c
febriramandha <febri.ramandha@gmail.com>
2022-08-05 09:35:03 +0700

    Upload Pertama

---
+++ index.php
@@ -0,0 +1,341 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<!--Import Google Icon Font-->
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<!--Import materialize.css-->
+	<link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
+
+	<!--My CSS-->
+	<link rel="stylesheet" href="css/style.css" />
+	
+	<!--AOS-->
+	<link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+	
+	<!-- Fontawesome CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+
+	<!--Let browser know website is optimized for mobile-->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+	<title>PT Catri Global Inframedia</title>
+	
+	<meta content="PT Catri Global Inframedia, Catri Global Inframedia, Catri Global, Catri" name="description">
+  <meta content="PT Catri Global Inframedia, Catri Global Inframedia, Catri Global, Catri" name="keywords">
+  <meta property="og:title" content="PT Catri Global Inframedia, Catri Global Inframedia, Catri Global, Catri" />
+	<meta property="og:description" content="Catri Global Inframedia, Catri Global, Catri">
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="https://www.catriglobal.com/">
+  <meta property="og:locale" content="id_ID">
+  
+  <meta name="google-site-verification" content="4cJop4S5cd4EptRh5VNjo1Dz_3MRiHrLKYAPaOCZhLI" />
+  
+	<link rel="shortcut icon" href="img/logo/cgi.png" type="image/x-icon">
+	
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-206795822-1">
+</script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-206795822-1');
+</script>
+</head>
+
+<body id="home" class="home scrollspy">
+
+	<!--Navbar-->
+	<div class="navbar-fixed">
+		<nav class="light-blue lighten-2">
+			<div class="container">
+				<div class="nav-wrapper">
+					<!--<a href="#home" class="brand-logo">Catri Global</a>-->
+					<a href="#home" class="logo mr-auto"><img src="img/logo/stempel4.png" style="z-index: 10; height: 60px;width: 180px; bottom: 50px; left: 25px;"></a>
+					<a href="#" data-target="mobile-nav" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+					<ul class="right hide-on-med-and-down">
+						<li><a href="#about">Tentang</a></li>
+						<li><a href="#clients">Partner</a></li>
+						<li><a href="#services">Services</a></li>
+						<li><a href="#portfolio">Portfolio</a></li>
+						<li><a href="#contact">Kontak</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	</div>
+	<!--Akhir Navbar-->
+
+	<!--Side Navbar-->
+	<ul class="sidenav" id="mobile-nav">
+		<li><a href="#about">Tentang</a></li>
+		<li><a href="#clients">Partner</a></li>
+		<li><a href="#services">Services</a></li>
+		<li><a href="#portfolio">Portfolio</a></li>
+		<li><a href="#contact">Kontak</a></li>
+	</ul>
+	<!--Akhir Side Navbar-->
+
+	<!--Slider-->
+	<div class="slider">
+		<ul class="slides">
+			<li>
+				<img src="img/portfolio/5.png">
+				<div class="caption left-align">
+					<h3>PT. Catri Global Inframedia</h3>
+					<h5 class="light grey-text text-lighten-3">"We Are Beyond The Dream"</h5>
+				</div>
+			</li>
+			<li>
+				<img src="img/slider/2.png">
+				<div class="caption right-align">
+					<h3>PT. Catri Global Inframedia</h3>
+					<h5 class="light grey-text text-lighten-3">"We Are Beyond The Dream"</h5>
+				</div>
+			</li>
+			<li>
+				<img src="img/portfolio/3.png">
+				<div class="caption center-align">
+					<h3>PT. Catri Global Inframedia</h3>
+					<h5 class="light grey-text text-lighten-3">"We Are Beyond The Dream"</h5>
+				</div>
+			</li>
+		</ul>
+	</div>
+	<!--Akhir Slider-->
+
+	<!--About Us-->
+	<section id="about" class="about scrollspy">
+		<div class="container" data-aos="fade-up" data-aos-duration="2000">
+			<div class="row">
+				<h3 class="center light grey-text text-darken-3">Tentang Kami</h3>
+				<p class="center"><strong>"We Are Beyond The Dream"</strong></p>
+				<div class="col m6 s12" data-aos="fade-right" data-aos-duration="1000">
+					<h5>PT. Catri Global Inframedia</h5>
+					<p align="justify">Perusahaan yang bergerak dibidang jasa pembuatan Aplikasi Web ataupun Android, digital marketing, pengadaan barang, event organiser, Construction, Procurement, and Engineering (EPC) and Infrastructure, dll.</p>
+					
+					<h5>VISI</h5>
+					<p align="justify">Menjadi perusahaan terdepan dibidang industri teknologi informasi dan infrastruktur ramah lingkungan dan inovatif, serta mampu bersaing secara global dengan mengedepankan akhlak, integritas, dan kepuasan pelanggan.</p>
+				</div>
+				<div class="col m6 s12" data-aos="fade-left" data-aos-duration="1000">
+					<p>WEB DEVELOPMENT</p>
+					<div class="progress">
+						<div class="determinate" style="width: 95%" data-aos="fade-right" data-aos-duration="2000" data-aos-delay="300"></div>
+					</div>
+					<p>MOBILE DEVELOPMENT</p>
+					<div class="progress">
+						<div class="determinate" style="width: 85%" data-aos="fade-right" data-aos-duration="2000" data-aos-delay="300"></div>
+					</div>
+					<p>DIGITAL MARKETING</p>
+					<div class="progress">
+						<div class="determinate" style="width: 90%" data-aos="fade-right" data-aos-duration="2000" data-aos-delay="300"></div>
+					</div>
+					<p>INFRASTRUKTUR</p>
+					<div class="progress">
+						<div class="determinate" style="width: 80%" data-aos="fade-right" data-aos-duration="2000" data-aos-delay="300"></div>
+					</div>
+					<p>CONSTRUCTION, PROCUREMENT, AND ENGINEERING (EPC)</p>
+					<div class="progress">
+						<div class="determinate" style="width: 80%" data-aos="fade-right" data-aos-duration="2000" data-aos-delay="300"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--Akhir About Us-->
+
+	<!--Clients-->
+	<div class="parallax-container scrollspy" id="clients">
+		<div class="parallax"><img src="img/slider/4.jpg"></div>
+
+		<div class="container clients" data-aos="fade-up" data-aos-duration="2000">
+			<h3 class="center light white-text">Client Kami</h3>
+
+			<div class="row">
+				<div class="col m6 s12 center" data-aos="zoom-in-right" data-aos-duration="2000" data-aos-delay="300">
+					<img src="img/clients/sonergy-black1.png">
+				</div>
+				<div class="col m6 s12 center" data-aos="zoom-in-left" data-aos-duration="2000" data-aos-delay="300">
+					<img src="img/clients/mgs-textiles2.png">
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--Akhir Clients-->
+
+	<!--Services-->
+	<section id="services" class="services grey lighten-3 scrollspy">
+		<div class="container" data-aos="fade-up" data-aos-duration="2000">
+			<div class="row">
+				<h3 class="center light grey-text text-darken-3">Pelayanan Kami</h3>
+
+				<div class="col m4 s12">
+					<div class="card-panel center" data-aos="flip-left" data-aos-duration="2000" data-aos-delay="100">
+						<i class="material-icons large">desktop_windows</i>
+						<h5>WEB DEVELOPMENT</h5>
+						<p>Kami menyediakan jasa pembuatan dan pengembangan Web Programming.</p>
+					</div>
+				</div>
+				<div class="col m4 s12">
+					<div class="card-panel center" data-aos="flip-left" data-aos-duration="2000" data-aos-delay="200">
+						<i class="material-icons large">developer_mode</i>
+						<h5>MOBILE APPS</h5>
+						<p>Kami menyediakan jasa pembuatan dan pengembangan Aplikasi Android.</p>
+					</div>
+				</div>
+				<div class="col m4 s12">
+					<div class="card-panel center" data-aos="flip-left" data-aos-duration="2000" data-aos-delay="300">
+						<i class="material-icons large">facebook</i>
+						<h5>DIGITAL MARKETING</h5>
+						<p>Kami menyediakan jasa Digital Marketing melalui Media Sosial seperti Instagram, Facebook, dll
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--Akhir Services-->
+
+	<!--Portfolio-->
+	<section id="portfolio" class="portfolio scrollspy">
+		<div class="container" data-aos="fade-up" data-aos-duration="2000">
+			<h3 class="center light grey-text text-darken-3">Portfolio</h3>
+			
+              <div class="carousel">
+                <a class="carousel-item" href="#one!"><img src="img/project/sonergy-index.png" class="responsive-img materialboxed"></a>
+                <a class="carousel-item" href="#two!"><img src="img/project/sonergy-old.png" class="responsive-img materialboxed"></a>
+                <a class="carousel-item" href="#three!"><img src="img/project/sonergy-home.png" class="responsive-img materialboxed"></a>
+                <a class="carousel-item" href="#four!"><img src="img/project/sonergy-mp.png" class="responsive-img materialboxed"></a>
+                <a class="carousel-item" href="#five!"><img src="img/project/sonergy-total.png" class="responsive-img materialboxed"></a>
+                <a class="carousel-item" href="#one!"><img src="img/project/mgs-login.png" class="responsive-img materialboxed"></a>
+                <a class="carousel-item" href="#two!"><img src="img/project/mgs-dashboard.png" class="responsive-img materialboxed"></a>
+                <a class="carousel-item" href="#three!"><img src="img/project/mgs-inputmasuk.png" class="responsive-img materialboxed"></a>
+                <a class="carousel-item" href="#four!"><img src="img/project/pv-1.png" class="responsive-img materialboxed"></a>
+                <a class="carousel-item" href="#five!"><img src="img/project/pv-2.png" class="responsive-img materialboxed"></a>
+                <a class="carousel-item" href="#one!"><img src="img/project/test1.jpg" class="responsive-img materialboxed"></a>
+              </div>
+  
+		</div>
+  
+	</section>
+	<!--Akhir Portfolio-->
+
+	<!--Contact-->
+	<section id="contact" class="contact grey lighten-3 scrollspy">
+		<div class="container" data-aos="fade-up" data-aos-duration="2000">
+			<h3 class="center light grey-text text-darken-3">Kontak Kami</h3>
+
+			<div class="row">
+				<div class="col m5 s12">
+					<div class="card-panel light-blue lighten-2 justify white-text">
+					        <h4 class="center">Hubungi</h4>
+					        
+						<i class="material-icons">email</i>
+						<!--<h5>Email</h5>-->
+						marketing@catriglobal.com <br>
+
+						<i class="material-icons">whatsapp</i>
+						<!--<h5>Whatsapp</h5>-->
+						0812-6153-9912 <br>
+						
+						<i class="material-icons">call</i>
+						<!--<h5>Telepon</h5>-->
+						021-27534458 <br>
+						
+						<i class="fab fa-instagram"></i> @catriglobal
+						<style>
+                            .fa-instagram { font-size: 25px; text-align: center; }
+                        </style>
+                        
+					</div>
+
+					<ul class="collection with-header">
+						<li class="collection-header center">
+							<h4>Kantor Kami</h4>
+						</li>
+						<li class="collection-item">
+							<h5>Aldeoz Building</h5>
+						</li>
+						<li class="collection-item">Jl. Warung Buncit Raya No.39, RT.01/RW.9, Kalibata, Kec. Pancoran.
+						</li>
+						<li class="collection-item">Jakarta Selatan, DKI Jakarta, Indonesia - 12740</li>
+					</ul>
+				</div>
+
+				<div class="col m7 s12">
+					<div class="card-panel s12">
+					    <div class="peta-responsive">
+					    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3965.9929885826746!2d106.8302714!3d-6.2646513!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f231b86d73c7%3A0x17e85c1417c96627!2sAldeoz%20Building!5e0!3m2!1sid!2sid!4v1630513735101!5m2!1sid!2sid" width="450" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+					    </div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</section>
+	<!--Akhir Contact-->
+	
+	<!--Whatsapp-->
+     <a target="_blank" href="https://wa.me/6281261539912?text=Halo%2C%20ada%20yang%20bisa%20kami%20bantu%20%3F"><img src="img/logo/wa-logo.png" alt="" style="z-index: 10; height: 60px;width: 60px;position: fixed;bottom: 50px; left: 25px;"></a>
+    <!--Akhir Whatsapp-->
+    
+    <!--<a href="#home">-->
+    <!--    <i class="medium material-icons" style="z-index: 10; height: 60px;width: 60px;position: fixed;bottom: 50px; right: 25px; transition: all 0.4s; display: flex;">arrow_upward</i>-->
+    <!--</a>-->
+    
+	<!--Footer-->
+	<footer class="light-blue lighten-2 white-text center">
+	    <div class="container">
+		Copyright &copy; PT. Catri Global Inframedia | 2021 - <?php echo date('Y'); ?>
+		</div>
+	</footer>
+	<!--Akhir Footer-->
+
+
+
+	<!--JavaScript at end of body for optimized loading-->
+	<script type="text/javascript" src="js/materialize.min.js"></script>
+
+	<script>
+		const sideNav = document.querySelectorAll('.sidenav');
+		M.Sidenav.init(sideNav);
+
+		const slider = document.querySelectorAll('.slider');
+		M.Slider.init(slider, {
+			indicators: false,
+			height: 500,
+			transition: 600,
+			interval: 3000
+		});
+
+		const parallax = document.querySelectorAll('.parallax');
+		M.Parallax.init(parallax);
+
+		const materialbox = document.querySelectorAll('.materialboxed');
+		M.Materialbox.init(materialbox);
+		
+		const carouSel = document.querySelectorAll('.carousel');
+		M.Carousel.init(carouSel, {
+		    numVisible: 7
+		});
+
+		const scroll = document.querySelectorAll('.scrollspy');
+		M.ScrollSpy.init(scroll, {
+			scrollOffset: 50
+		});
+	</script>
+	
+	<!--AOS-->
+	<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+        AOS.init();
+    </script>
+
+</body>
+
+<!-- Fontawesome JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js"></script>
+
+</html>
